21382a4f5744d13634de3ced50576902f37b8eca
Juan Carlos Fernández García <jucarlos@jccm.es>
2025-04-09 12:27:30 +0200

    Tenemos el menú de opciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
+  // home
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then( m => m.HomeModule )
+  },
 
-
-  // buena del auth
+  //  auth
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
   },
-
-  // products.
+  // products
+  {
+    path: 'products',
+    loadChildren: () => import('./products/products.module').then( m => m.ProductsModule)
+  },
 
 
   {
--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListProductsComponent } from './pages/list-products/list-products.component';
+import { DetailsProductComponent } from './pages/details-product/details-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    children: [
+      {
+        path: 'products-list',
+        component: ListProductsComponent
+      },
+      {
+        path: 'detail-product/:id',
+        component: DetailsProductComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'products-list'
+      }
+    ]
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
