c093cc70f4ccd88301510e567fcec5d308470fd1
Juan Carlos Fernández García <jucarlos@jccm.es>
2025-04-10 10:44:29 +0200

    Empezando con formularios reactivos

--- README.md
+++ README.md
@@ -14,6 +14,13 @@ Opciones de la aplicación:
         - servicios
         - guads ( controlar accesos a rutas )
         - paginas de login y de registro ( formularios reactivos y no templates )
+
+        - Usaremos autenticación con JWT , que tiene una duración de 2 horas.
+        cada vez que iniciemos la aplicacion el servidor nos dirá si el token es correcto.
+
+        
+
+
     - products
         - servicios,
         - páginas ( lista de productos, detalle de un producto )
--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
 
+  // Formularios por template. El trabajo se hace en mayor parte en el html
+  
+  // Formularios reactivos. El trabajo se hace en el TS.
+  // 1. Decir a angular que incorpore el módulo de formularios reactivos ReactiveFormsModule.
+  // 2. Incluir en el componente de ts, el servicio formBuilder que nos ayuda a la creación.
+  // 3. Definir en el componente de ts el formulario ( nombre, apellidos, email, edad ) y validaciones
+  // 4. Mapear la configuración del componente de ts al html.
+
+
+  
+
+
+
+
+
+
 }
